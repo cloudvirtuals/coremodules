4d7d5318897ee8c341f799ccaf3c9a4eda8f1b95
Yuvaraj Gopal <yuvarajgopal@Yuvarajs-MacBook-Pro.local>
2024-02-25 22:38:50 -0600

    update

--- modules/adf/adf.tf
+++ modules/adf/adf.tf
@@ -1,13 +1,8 @@
-# main.tf
-
-provider "azurerm" {
-  features = {}
-}
 
 # Create main resource group
 resource "azurerm_resource_group" "rgdata01" {
   name     = "adf-${var.orgname}-${var.env}-rg"
-  location = "${var.default_location}"
+  location = "${var,default_location}"
 }
 
 # Reference to an existing Azure Subnet
