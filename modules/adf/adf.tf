c7535d24bba172e28de7aa112c84517a3f750f33
yuvaraj.cloudvirtuals@gmail.com <yuvaraj.cloudvirtuals@gmail.com>
2024-02-26 18:32:29 -0600

    updatedadf

--- modules/adf/adf.tf
+++ modules/adf/adf.tf
@@ -19,19 +19,28 @@ resource "azurerm_data_factory" "adf" {
   location            = azurerm_resource_group.rgdata01.location
 }
 
+resource "azurerm_storage_account" "adfstgacc" {
+  name                     = "adfstg-${var.orgname}"
+  resource_group_name      = azurerm_resource_group.rgdata01.name
+  location                 = azurerm_resource_group.rgdata01.location
+  account_kind             = "BlobStorage"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
 # Create a Self-Hosted Integration Runtime in an existing subnet
 resource "azurerm_data_factory_managed_private_endpoint" "adf_endpoint" {
   name                = "adf-integration-endpoint"
-  data_factory_name   = azurerm_data_factory.adf.name
+  data_factory_id   = azurerm_data_factory.adf.id
   resource_group_name = azurerm_resource_group.rgdata01.name
-
+  target_resource_id = azurerm_storage_account.adfstgacc.id
   subnet_id = data.azurerm_subnet.PaaS-subnet1.id
 }
 
 # Create a Self-Hosted Integration Runtime linked to the private endpoint
-resource "azurerm_data_factory_managed_private_endpoint_connection" "adf_connection" {
-  name                     = "adf-integration-endpoint-connection"
-  data_factory_name        = azurerm_data_factory.adf.name
-  resource_group_name      = azurerm_resource_group.rgdata01.name
-  managed_private_endpoint = azurerm_data_factory_managed_private_endpoint.adf_endpoint.id
-}
+#resource "azurerm_data_factory_managed_private_endpoint_connection" "adf_connection" {
+#  name                     = "adf-integration-endpoint-connection"
+#  data_factory_name        = azurerm_data_factory.adf.name
+#  resource_group_name      = azurerm_resource_group.rgdata01.name
+#  managed_private_endpoint = azurerm_data_factory_managed_private_endpoint.adf_endpoint.id
+#}
