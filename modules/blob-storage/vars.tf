a556b6d8c81b17eae51114a5a06ccaba85e5aa97
yuvaraj.cloudvirtuals@gmail.com <yuvaraj.cloudvirtuals@gmail.com>
2024-02-29 19:02:40 -0600

    update

--- modules/blob-storage/vars.tf
+++ modules/blob-storage/vars.tf
@@ -1,20 +1,18 @@
+variable "env" {
+  type = string
+}
 variable "location" {
-  sensitive = true
   type = string
 }
 variable "rgname" {
-  sensitive = true
   type = string
 }
 variable "orgname" {
-  sensitive = true
   type = string
 }
 variable "adfstgname" {
-  sensitive = true
   type = string
 }
 variable "adfstgcont01" {
-  sensitive = true
   type = string
 }
